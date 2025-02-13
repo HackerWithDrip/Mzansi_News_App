748d8aad4370802976b5fc4beeff670cd52b4742
Lionel <lraseemela@gmail.com>
2025-02-13 09:41:27 +0200

    added article_model

---
+++ local_news_app/lib/models/article_model.dart
@@ -0,0 +1,17 @@
+class ArticleModel {
+  String? title;
+  String? description;
+  String? url;
+  String? urlToImage;
+  //String? publishedAt;
+  String? content;
+  String? author;
+
+  ArticleModel(
+      {this.title,
+      this.description,
+      this.url,
+      this.urlToImage,
+      this.content,
+      this.author});
+}
